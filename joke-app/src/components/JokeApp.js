e8ddb6e6343818e8809240c20bdb9bdc407ac668
Your Name <tan.ledang@digitalcrafts.com>
2023-02-04 13:51:07 -0500

    Add useEffect hook to fetch jokes on component render

--- joke-app/src/components/JokeApp.js
+++ joke-app/src/components/JokeApp.js
@@ -1,11 +1,14 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import Joke from './Joke';
 
 const JokeApp = () => {
     const [jokes, setJokes] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    useEffect(() => fetchJoke, [])
+
     const fetchJoke = async () => {
+        console.log("fetching jokes")
         setIsLoading(true);
         const url = 'https://official-joke-api.appspot.com/random_ten';
         fetch(url)
@@ -22,7 +25,7 @@ const JokeApp = () => {
             {isLoading ? <p>"Loading..." </p>: jokes.map(joke => 
             <Joke setup={joke.setup} punchline={joke.punchline} key={joke.id}/>)}
 
-            {!isLoading && <button onClick={fetchJoke}>New Joke</button>}    
+            {!isLoading && <button onClick={fetchJoke}>New Jokes</button>}    
         </div>
     )
 }
